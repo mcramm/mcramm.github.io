9be1d543af18abace9f1c4907596174c3ea88713
mcramm <GMCramm@gmail.com>
2010-06-29 16:27:03 -0600

    Comment out ip bind

--- mcramm.rb
+++ mcramm.rb
@@ -11,7 +11,7 @@ set :app_file, __FILE__
 set :root, File.dirname(__FILE__)
 set :views, "views"
 set :public, "static"
-set :bind, '10.131.25.4'
+# set :bind, '10.131.25.4'
 
 configure do 
   Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.config'))
