34ba4471861a60fe666fdabeb98207fae15f08ec
mcramm <GMCramm@gmail.com>
2010-07-21 09:32:51 -0600

    Get tweets after page is loaded

--- static/javascripts/script.js
+++ static/javascripts/script.js
@@ -1,18 +1,30 @@
 GITHUB_USERNAME='mcramm';
+function getTimeline(){
+    $.ajax({ 
+            url: "/tweets.json", 
+            context: document.body,
+            dataType: 'json',
 
-function getTweet(value){
-	var current_tweet = parseInt(document.getElementById('current-tweet').innerHTML);
-	var tweet_limit = parseInt(document.getElementById('limit').innerHTML);
-	var new_tweet = current_tweet + value;
-	if (new_tweet >= 1 && new_tweet < tweet_limit+1){
-		if(new_tweet == 1){
-			document.getElementById('prev-tweet').innerHTML = 'prev';
-		}
-		if(new_tweet == tweet_limit){
-			document.getElementById('next-tweet').innerHTML = 'next';
-		}
-		document.getElementById('tweet-'+current_tweet).style.display = 'none';
-		document.getElementById('tweet-'+new_tweet).style.display = 'block';
-		document.getElementById('current-tweet').innerHTML = new_tweet;
-	}
+            success: function(response){
+                place(response);
+            },  
+            error: function(XMLHttpRequest, textStatus, errorThrown){
+                console.log(errorThrown);
+            }   
+            }); 
+
 }
+
+function place(json){
+    str = "";
+    for(var i in json.tweets){
+        tweet = json.tweets[i];
+        str += "<p class='tweet'>" + tweet.text + " <a href='http://twitter.com/cramm/statuses/"+tweet.id+"'class='date'>("+tweet.date+")</a></p>";
+    }           
+
+    $('#tweets').html(str);
+
+}
+$('document').ready( function(){
+    getTimeline();
+});
