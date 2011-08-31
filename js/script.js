ef6d75d607d2dbfbed67b0d0fb587fb96394b53d
mcramm <GMCramm@gmail.com>
2011-08-31 10:15:58 -0600

    adding something special to happen at the end of the countdown

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 function refreshTime() {
-
   var now = new Date();
   var mil = window.then.getTime() - now.getTime();
   if( mil <= 0) {
+    resetClock();
+    resetPercentage();
+    hideClock();
+    setSpecial();
     return;
   }
   var sec = mil/1000;
@@ -22,13 +25,7 @@ function refreshTime() {
   running_diff = running_diff*1000 + sec*1000;
   mil = Math.floor(mil - running_diff);
 
-
-  $('#days').html(day);
-  $('td#hours').html(hour);
-  $('#minutes').html(min);
-  $('#seconds').html(sec);
-  $('#mil').html(mil);
-
+  setClock(day, hour, min, sec, mil);
 
   var steps = window.then.getTime() - window.startDate.getTime();
   var diff = now.getTime() - window.startDate.getTime();
@@ -37,6 +34,8 @@ function refreshTime() {
   $('#percentage').html(percentage);
 
   adjustOverlay(percentage/100);
+
+  setTimeout( "refreshTime()", 40 );
 }
 
 function adjustOverlay(per) {
@@ -50,12 +49,55 @@ function adjustOverlay(per) {
 }
 
 
+function setClock(day, hour, min, sec, mil){
+  $('#days').html(day);
+  $('td#hours').html(hour);
+  $('#minutes').html(min);
+  $('#seconds').html(sec);
+  $('#mil').html(mil);
+}
+
+function resetClock(){
+  setClock(0,0,0,0,0);
+}
+
+function resetPercentage(){
+  $('.overlay').css('top', 0);
+  $('#percentage').html(100);
+}
+
+function setSpecial() {
+  $.ajax({
+    url: "/lyrics.html",
+    success: function(data) {
+      $('div#holder').html(data);
+      $('div#info').html("Daft Punk - \"Digital Love\"");
+      showSpecial();
+    }
+  });
+}
+
+function hideClock(){
+  $('div#clock-wrap').animate({
+    height: 'toggle'
+  }, 1000);
+}
+
+function showSpecial(){
+  $('div#holder-wrap').animate({
+    height: 'toggle'
+  }, 1000);
+
+  $('div#explanation-wrap').animate({
+    height: 'toggle'
+  }, 1000);
+
+}
+
+
 $(document).ready( function() {
   window.then = new Date(2011,08,02,20,44,00);
   window.startDate = new Date(2011,07,26,20,44,00);
 
   refreshTime();
-
-  setInterval( "refreshTime()", 40 );
-
 });
